b174dc8253c0ebf3e4d29075e6ff52267ef554b2
Seyit Murat Kaya <murat.kaya1269@gmail.com>
2022-02-20 00:36:55 +0300

    add pause and reset

--- PomodoroIOS/ContentView.swift
+++ PomodoroIOS/ContentView.swift
@@ -10,28 +10,25 @@ import UserNotifications
 
 
 struct ContentView: View {
-    @Environment(\.scenePhase) var scenePhase
     
-    @State var studyLength = 1 // minutes
+    @State var studyLength = 25 * 60
     @State var breakLength = 0
     @State var timeCount = 0
     @State var minutes = 0
     @State var seconds = 0
-    @State var isBreakeTime: Bool = true
-    @State var isInBackground: Bool = false
+    @State var isBreakeTime: Bool = false
+    @State var timerIsPaused: Bool = false
+    @State var IsRunning:Bool = false
     @State var timerValue:Int = 0
     @State var pomodoroCount = 0
-    @State var timerIsPaused: Bool = true
     @State var timer : Timer?
     
     func secondsToMinutesSeconds(_ seconds: Int) -> (Int, Int) {
         return ((seconds % 3600) / 60, (seconds % 3600) % 60)
     }
-    func startPomodoro(){
-        isBreakeTime = false
-        
-        timerIsPaused = false
-        timeCount = studyLength * 60
+    
+    func startTimer(length:Int){
+        IsRunning = true
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){ tempTimer in
             if timeCount > 0{
@@ -39,62 +36,57 @@ struct ContentView: View {
                 timeCount -= 1
             }else if timeCount == 0{
                 timer?.invalidate()
-                startBreak()
+                if isBreakeTime{
+                    startStudy()
+                }else{
+                    pomodoroCount += 1
+                    startBreak()
+                }
             }
-            
         }
-    }
-    func startBreak(){
-        pomodoroCount += 1
-        isBreakeTime = true
-        if pomodoroCount % 4 == 0{
-            breakLength = 20 * 60 //minutes
-        }else{
-            breakLength = 5 * 60 // minutes
-        }
-        
-        timeCount = breakLength
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){ tempTimer in
-            if timeCount > 0{
-                (minutes,seconds) = secondsToMinutesSeconds(timeCount)
-                timeCount -= 1
-            }else if timeCount == 0{
-                timer?.invalidate()
-                startPomodoro()
-            }
-            
-        }
-        
     }
     
-    func stopTimer(){
+    func startStudy(){
+        if timerIsPaused{
+            if isBreakeTime{
+                startTimer(length: timeCount)
+            }else{
+                isBreakeTime = false
+                startTimer(length: timeCount)
+            }
+        }else{
+            isBreakeTime = false
+            timeCount = studyLength
+            startTimer(length: timeCount)
+        }
+        timerIsPaused = false
+    }
+    
+    func startBreak(){
+        timerIsPaused = false
+        isBreakeTime = true
+        if pomodoroCount % 4 == 0{
+            breakLength = 20 * 60
+        }else{
+            breakLength = 5 * 60
+        }
+        timeCount = breakLength
+        startTimer(length: timeCount)
+    }
+    
+    func pauseTimer(){
+        IsRunning = false
         timerIsPaused = true
         timer?.invalidate()
+        timeCount = minutes * 60 + seconds - 1
     }
+    
     func resetTimer(){
-        timer?.invalidate()
-    }
-    func requestPermission(){
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { success, error in
-            if success {
-                print("success")
-            }else if let e = error{
-                print(e.localizedDescription)
-            }
+        if isBreakeTime {
+            startBreak()
+        }else{
+            startStudy()
         }
-    }
-    func sendNotification(secondsLeft:Int){
-        let content = UNMutableNotificationContent()
-        content.title = isBreakeTime ? "Break Time" : "Study Time"
-        content.subtitle = isBreakeTime ? "\(breakLength) minutes break" : ""
-        content.sound = UNNotificationSound.default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: Double(secondsLeft), repeats: false)
-        
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request)
     }
     
     var body: some View {
@@ -111,35 +103,17 @@ struct ContentView: View {
                 }.foregroundColor(.white)
                 Spacer()
                 HStack{
-                    if timerIsPaused{
-                        ButtonView(text: "Start",function: startPomodoro)
+                    if IsRunning{
+                        ButtonView(text: "Pause",function: pauseTimer)
                     }else{
-                        ButtonView(text: "Stop",function: stopTimer)
+                        ButtonView(text: "Start",function: startStudy)
                     }
                     ButtonView(text: "Reset", function: resetTimer)
                 }
                 Spacer()
-                
-                
             }
         }
         .ignoresSafeArea(.all)
-        .onAppear {
-            requestPermission()
-        }
-        .onChange(of: scenePhase) { newValue in
-            if newValue == .active{
-                print("active")
-                if isInBackground{
-                    
-                }
-            }else if newValue == .inactive{
-                print("inactive")
-            }else if newValue == .background{
-                print("background")
-                sendNotification(secondsLeft: timeCount)
-            }
-        }
     }
 }
 
